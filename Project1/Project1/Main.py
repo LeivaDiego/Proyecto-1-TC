7c6f09e0230f3bfa2796391bc6f863382839ccc6
LeivaDiego <lei21752@uvg.edu.gt>
2023-09-17 15:18:19 -0600

    main module implementation

---
+++ Project1/Project1/Main.py
@@ -0,0 +1,43 @@
+import io
+from shuntingYard import ShuntingYard
+from syntaxTree import SyntaxTree
+from AFNsimulator import thompson_from_tree, State, simulate_afn
+
+converter = ShuntingYard()
+tree_maker = SyntaxTree()
+
+
+
+with io.open('regex.txt', 'r', encoding='utf-8') as file:
+    lines = [line.strip().replace(" ", "") for line in file]
+
+for index, infix in enumerate(lines):
+    # Conversion de infix a postfix
+    print(f"Original: {infix}")
+    postfix = converter.infixToPostfix(infix)
+    print(f"Postfix: {postfix}\n")
+
+    # Construccion y visualizacion del arbol sintactico
+    root = tree_maker.build_tree(postfix)
+    tree_dot = tree_maker.visualize_tree(root)
+    tree_dot.render(f'arbol_sintactico_{index+1}', view = True, cleanup=True)
+
+    # Construccion y visualizacion del AFN
+    State.state_counter = 0
+    afn = thompson_from_tree(root)
+    afn.assign_state_numbers()
+    afn_dot = afn.visualize_afn()
+    afn_dot.render(f'afn_{index+1}', view = True, cleanup=True)
+    
+
+    # Evaluacion de la cadena 
+    w = input(f"Ingrese una cadena para probrar el AFN:")
+
+    is_accepted_afn = simulate_afn(afn, w)
+
+    if is_accepted_afn:
+        print(f"La cadena '{w}' es aceptada por el AFN\n")
+    else:
+        print(f"La cadena '{w}' no es aceptada por el AFN\n")
+
+    pause = input("Presione enter para continuar")
