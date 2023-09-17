43478398abe4f3c847dfb5be0d17483714fb3130
LeivaDiego <lei21752@uvg.edu.gt>
2023-09-17 15:20:04 -0600

    AFD simul Testing

--- Project1/Project1/Main.py
+++ Project1/Project1/Main.py
@@ -2,6 +2,7 @@ import io
 from shuntingYard import ShuntingYard
 from syntaxTree import SyntaxTree
 from AFNsimulator import thompson_from_tree, State, simulate_afn
+from AFDsimulator import convert_to_afd, simulate_afd
 
 converter = ShuntingYard()
 tree_maker = SyntaxTree()
@@ -30,14 +31,25 @@ for index, infix in enumerate(lines):
     afn_dot.render(f'afn_{index+1}', view = True, cleanup=True)
     
 
+    afd = convert_to_afd(afn)
+    afd_dot = afd.visualize_afd()
+    afd_dot.render(f'afd_{index+1}', view = True, cleanup = True)
+
+
     # Evaluacion de la cadena 
     w = input(f"Ingrese una cadena para probrar el AFN:")
 
     is_accepted_afn = simulate_afn(afn, w)
+    is_accepted_afd = simulate_afd(afd, w)
 
     if is_accepted_afn:
         print(f"La cadena '{w}' es aceptada por el AFN\n")
     else:
         print(f"La cadena '{w}' no es aceptada por el AFN\n")
+
+    if is_accepted_afd:
+        print(f"La cadena '{w}' es aceptada por el AFD\n")
+    else:
+        print(f"La cadena '{w}' no es aceptada por el AFD\n")
 
     pause = input("Presione enter para continuar")
